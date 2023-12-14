3f35428467186b12af1f21529094f9700319fc3a
e1turin <ivturnik2003@gmail.com>
2023-12-14 16:12:46 +0300

    init template

---
+++ src/test/kotlin/io/github/e1turin/itmochan/ItmochanApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.e1turin.itmochan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItmochanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
