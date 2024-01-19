4852193574c3d869a64838c6d70a129549b460a7
Kimiega <93886405+Kimiega@users.noreply.github.com>
2024-01-19 04:41:29 +0300

    fix schema

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS "Users" (
 );
 
 CREATE TABLE IF NOT EXISTS "Roles" (
-                         role_id integer PRIMARY KEY,
+                         role_id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                          name varchar(255) NOT NULL UNIQUE,
                          description text NOT NULL
 );
