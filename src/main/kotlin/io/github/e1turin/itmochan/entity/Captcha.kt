708e223045d26ee3a027488791ff527791419992
Kimiega <93886405+Kimiega@users.noreply.github.com>
2024-01-19 04:29:49 +0300

    fix typo captcha

--- src/main/kotlin/io/github/e1turin/itmochan/entity/Captcha.kt
+++ src/main/kotlin/io/github/e1turin/itmochan/entity/Captcha.kt
@@ -4,7 +4,7 @@ import org.springframework.data.relational.core.mapping.Table
 
 @Table("Captcha")
 data class Captcha(
-    val capthchaId : Long,
+    val captchaId : Long,
     val answer : String?,
     val pictureId : Long,
 )
