7317ce252af04fccd43b25fd84b60ae68752e585
Kimiega <dmbelitmo@mail.ru>
2025-01-23 08:18:01 +0300

    remove unnecessary things

--- src/main/kotlin/io/github/secsdev/itmochan/response/FileResponse.kt
+++ src/main/kotlin/io/github/secsdev/itmochan/response/FileResponse.kt
@@ -1,18 +1,6 @@
 package io.github.secsdev.itmochan.response
 
 import io.github.secsdev.itmochan.entity.File
-import io.github.secsdev.itmochan.entity.Picture
-import io.github.secsdev.itmochan.entity.Video
-
-class PictureDTO(
-    val picture : Picture,
-    val byteArray: ByteArray,
-)
-
-class VideoDTO(
-    val video : Video,
-    val byteArray: ByteArray,
-)
 
 class FileDTO(
     val file: File,
